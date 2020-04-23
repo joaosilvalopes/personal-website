62f5cb094e1801288ee25958dabf6a144e68c68e
joaosilvalopes <jpgdsl@gmail.com>
2020-04-23 15:08:17 +0100

    dawadw

---
+++ build/precache-manifest.d232d95681a2a5510eb38fb3c469f94b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3945f75b6f66b5ce8a58766f7a21c89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e54496f5a160c649dede",
+    "url": "/static/js/2.5e5aceeb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5e5aceeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214ac28182950e4382fe",
+    "url": "/static/js/main.1ab97d25.chunk.js"
+  },
+  {
+    "revision": "f820e8b49ef618b89d40",
+    "url": "/static/js/runtime-main.24c0de7d.js"
+  }
+]);
